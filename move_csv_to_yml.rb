ea0a676ead78fe012c02bde6bb4829657224013d
Ryan Mark <ryanmark2014@u.northwestern.edu>
2018-03-24 18:20:35 -0400

    move problem list csv to yaml file

---
+++ move_csv_to_yml.rb
@@ -0,0 +1,17 @@
+require 'csv'
+require 'yaml'
+
+tests = CSV.read('list.csv', headers: true, header_converters: :symbol, converters: :all)
+
+categories = Hash.new { |h, k| h[k] = [] }
+
+tests.each do |test|
+  new_entry = {}
+  new_entry[:name] = test[:name]
+  new_entry[:problem_file] = test[:problemfile]
+  new_entry[:spec_file] = test[:specfile]
+  new_entry[:solution_file] = test[:solutionfile]
+  categories[test[:category]] << new_entry
+end
+
+File.open("list.yml", "w+") { |f| f.write(categories.to_yaml) }
